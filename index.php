472e2b84453797734c388c9d11714d1a85836aa2
Skibidi <dasha.panteleymonova@gmail.com>
2026-09-21 13:04:02 +0300

    Lab 2

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
-  echo "<h1>Hello World! Це моя перша PHP-програма.</h1>";
-  phpinfo();
+$appName = "Task Manager";
+$taskTitle = "Вивчити основи PHP";
+$taskTimeEstimate = 3;
 ?>
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $appName ?></title>
+</head>
+<body>
+    <header>
+        <h1><?= $appName ?></h1>
+    </header>
+    <main>
+        <h2>Список завдань</h2>
+        <ul>
+            <li>Завдання: <?= $taskTitle ?></li>
+            <li>Очікуваний час: <?= $taskTimeEstimate ?> год.</li>
+        </ul>
+    </main>
+</body>
+</html>
